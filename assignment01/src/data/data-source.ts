bc5cf100b32ee07725da45edda278710bbf1ba07
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2025-04-13 17:08:06 +0400

    [UPDATE] Updated the variables to be taken from the .env

--- assignment01/src/data/data-source.ts
+++ assignment01/src/data/data-source.ts
@@ -1,13 +1,14 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { Message } from './entities/nuts-message.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'username',
-  password: 'password',
-  database: 'valhalla',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'username',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'valhalla',
   entities: [Message],
-  synchronize: true, // false for prod
+  synchronize: true, // False for Prod
 });
