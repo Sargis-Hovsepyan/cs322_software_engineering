80759b11dfc8f0014fd1d9a62ef5801dc2aae167
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2025-04-13 17:09:45 +0400

    [UPDATE] Updated the variables to be taken from the .env

--- assignment01/src/data/data-source.ts
+++ assignment01/src/data/data-source.ts
@@ -4,11 +4,11 @@ import { Message } from './entities/nuts-message.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'username',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'valhalla',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [Message],
   synchronize: true, // False for Prod
 });
